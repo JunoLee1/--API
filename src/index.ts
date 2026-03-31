73d743db4acb6c41085eb3eb710047fc6eb5330b
준오 <juno@jun-oui-noteubug.local>
2026-03-31 12:35:32 +0900

    <feat> frame for routes

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {Router} from "express"
+
+const router = Router()
+
+router.use(
+    //유저 CRUD (/api/users)
+    //시즌 CRUD (/api/seasons)
+    //경기 CRUD (/api/matches)
+    //선수 CRUD (/api/players)
+)
+
+export default router
