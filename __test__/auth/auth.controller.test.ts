33ebb1e76c05596dace715804d65cd53484ba4a3
준오 <juno@jun-oui-noteubug.local>
2026-04-02 11:10:28 +0900

    <feat/fix> login controller/ signUp Controller
    <signUp>
    1. 400 status -> 500 status
    2. rename create to signUp
    
    <login>
    1. if it occurs Internal server Error, check 500 status and the message
    
    2. check It is call service function.

--- __test__/auth/auth.controller.test.ts
+++ __test__/auth/auth.controller.test.ts
@@ -3,26 +3,24 @@ import AuthController from "../../src/auth/auth.controller";
 const authService = {
   login: jest.fn(),
   logout: jest.fn(),
-  create: jest.fn(),
+  signUp: jest.fn(),
 } as any; // 임시
 
 const controller = new AuthController(authService);
 describe("인증 로직 테스트 - registry service", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   test("respond 201 when registry success", async () => {
-    const result = jest.spyOn(authService, "create").mockResolvedValue({
-      id: 1,
-      email: "example@test.com",
-      password: "12345678",
-    });
     const req = { body: { email: "test@test.com" } };
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     };
 
-    await controller.create(req as any, res as any);
-    expect(authService.create).toHaveBeenCalled();
+    await controller.signUp(req as any, res as any);
     expect(res.status).toHaveBeenCalledWith(201);
+    expect(authService.signUp).toHaveBeenCalled();
   });
   test("respond 400 when registry fail", async () => {
     const req = { body: { email: "test@test.com" } };
@@ -30,15 +28,45 @@ describe("인증 로직 테스트 - registry service", () => {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
     };
-    
-    authService.create.mockRejectedValue(new Error("Something Wrong"))
 
-    await controller.create(req as any, res as any)
-    expect(res.status).toHaveBeenCalledWith(400)
+    authService.signUp.mockRejectedValue(new Error("Something Wrong"));
+
+    await controller.signUp(req as any, res as any);
+    expect(res.status).toHaveBeenCalledWith(500);
     expect(res.json).toHaveBeenCalledWith({
-        message: "Something Wrong"
-    })
+      message: "Something Wrong",
+    });
   });
 });
 //=========================================================================
 
+describe("인증 로직 테스트 - login controller", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("로그인에 성공한 경우, 200 상태코드 던지기", async () => {
+    const req = { body: { email: "test@test.com", password: "12345567" } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    authService.login.mockResolvedValue({ email: "test@test.com" });
+    await controller.login(req as any, res as any);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ email: "test@test.com" });
+  });
+  test("로그인에 실패한 경우, 500에러와 '알수없는 에러' 메시지 던지기", async () => {
+    const req = { body: { email: "test@test.com", password: "12345567" } };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    authService.login.mockRejectedValue(new Error("Something Wrong"));
+    await controller.login(req as any, res as any);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Something Wrong",
+    });
+  });
+});
+//======================================================================================
