0b7d553965ef089ded3570d22f6c264f41dfc401
준오 <juno@jun-oui-noteubug.local>
2026-04-05 12:04:28 +0900

    <feat/ controller-test> 관리자들 찾기/ 단일 관리자 수정
    1. 서비스로직 함수 호출 성공시 200 테스트 확인
    
    2. 단일 관리자
    	- 로그인 되지 않는 유저 401 에러
    	- 서비스 로직 함수 호출 성공시 200

--- __test__/auth/auth.controller.test.ts
+++ __test__/auth/auth.controller.test.ts
@@ -6,6 +6,7 @@ const authService = {
   signUp: jest.fn(),
   findAdvisorById: jest.fn(),
   findAdvisors: jest.fn(),
+  updatesAdvisor: jest.fn()
 } as any; // 임시
 
 const controller = new AuthController(authService);
@@ -148,7 +149,6 @@ describe("인증 컨트롤러 테스트 - 관리자 조회", () => {
     expect(res.json).toHaveBeenCalled();
   });
 });
-
 //======================================================================================
 describe("인증 컨트롤러 테스트 - 관리자들 조회", () => {
   beforeEach(() => {
@@ -185,13 +185,102 @@ describe("인증 컨트롤러 테스트 - 관리자들 조회", () => {
       message: "FORBIDDEN",
     });
   });
-  /*
+  
   test("해당 서비스 로직이 호출이 된다면 200", async () => {
     const req = {
+      user:{role:"SUPER_ADMIN"},
       query: { take: 10, skip: 0 },
     };
+    const res  = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    }
+    authService.findAdvisors({
+      user:{role:"SUPER_ADMIN"},
+      query: { take: 10, skip: 0 },
+    })
+    await controller.findAdvisors(req as any, res as any)
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalled()
   });
-
-  test("알수없는 에러 인 경우, 500 과 Something Wrong", async () => {});
-  */
+  test("알수없는 에러 인 경우, 500 과 Something Wrong", async () => {
+    const req = {
+      user:{role:"SUPER_ADMIN"},
+      query: { take: 10, limit: 0 },
+    }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    }
+   
+    authService.findAdvisors.mockRejectedValue(new Error("Something Wrong"))// 예상 결과값
+    await controller.findAdvisors(req as any, res as any)//해당 함수 호출
+    expect(res.status).toHaveBeenCalledWith(500)// 결과 값 비교
+    expect(res.json).toHaveBeenCalledWith({
+      message:"Something Wrong"
+    })
+  });
 });
+//======================================================================================
+describe("인증 컨트롤러 테스트 - 단일 관리자 정보 수정", () => {
+  
+  // 로그인 되어있지 않는 경우 401
+  test("인증 되지 않는 유저 인경우 401 에러 코드 및, 에러 메시지 동일 확인", async() =>{
+    const req = {
+      user:{
+        id : null
+      }
+    }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+    await controller.updatesAdvisor(req, res)
+    expect(res.status).toHaveBeenCalledWith(401)
+    expect(res.json).toHaveBeenCalledWith({
+      message: "UNAUTHORIZED"
+    })
+  })
+
+  /*
+  // 알 수 없는 에러인경우 500
+  test("", async() =>{})
+*/
+  // 성공적으로 서비스 로직 값이 호출된 경우 200
+  test("성공적으로 서비스 로직 값이 호출된 경우 200 와 성공메시지 던지기", async() =>{
+    const req = {
+      user:{
+        id:1,
+        username:"juno",
+        teamname:"mate fc"
+      },
+      body:{
+        username:"juno",
+        teamname:"mate fc"
+      }
+    }
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    }
+    console.log(1)
+    authService.updatesAdvisor.mockResolvedValue({
+      username:"juno",
+      teamname:"mate fc"
+    })
+    console.log(12)
+    await controller.updatesAdvisor(req, res)
+    expect(res.status).toHaveBeenCalledWith(200)
+    expect(res.json).toHaveBeenCalledWith({
+      message:"successfully modified information"
+    })
+  }) 
+})
+//======================================================================================
+/*
+describe("인증 컨트롤러 테스트 - 다수의 관리자 정보 수정")
+//======================================================================================
+describe("인증 컨트롤러 테스트 - 단일 관리자 삭제")
+//======================================================================================
+describe("인증 컨트롤러 테스트 - 다수 관리자 삭제")
+*/
