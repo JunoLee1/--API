12d032c019aaaaabdcdce6504bef65c148d807db
준오 <juno@jun-oui-noteubug.local>
2026-04-05 13:57:30 +0900

    <feat> auth controller
    1. 단일 관리자 찾기
    	- 로그인 안되어져 있으면 401 에러
    	- 권한 없으면 403 에러
    	- 알수 없는 에러 인경우 500
    	- 성공적으로 서비스 로직 호출된다면 200
    
    2. 다수 관리자 찾기
    	- 로그인 안되어져 있다면 401
    	- 권한 없으면 403
    	- 알 수 없는 에러인 경우 500
    	- 성공적으로 서비스 로직 호출된다면 200

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import AuthService from "./auth.service";
-import {LoginInput} from "./auth.DTO"
+import { LoginInput } from "./auth.DTO";
 export default class AuthController {
   constructor(private service: AuthService) {}
   async signUp(req: any, res: any) {
@@ -12,53 +12,79 @@ export default class AuthController {
   }
   //=================================================================================================================================================================================
 
-  async login(req: any, res: any) {//TODO: TYPE CONVERT
+  async login(req: any, res: any) {
+    //TODO: TYPE CONVERT
     try {
       const { email, password } = req.body as LoginInput;
       const result = await this.service.login({ email, password });
       return res.status(200).json(result);
     } catch (error) {
-      console.log(error);
       return res.status(500).json({ message: "Something Wrong" });
     }
   }
   //=================================================================================================================================================================================
-  async findAdvisorById(req: any, res: any) {//TODO: TYPE CONVERT
+  async findAdvisorById(req: any, res: any) {
+    //TODO: TYPE CONVERT
     try {
-        const{ id } = req.params
-        if(!req.user){
-          return res.status(401).json({message:"UNAUTHORIZED"})
-        }
-        const advisor = await this.service.findAdvisorById(id)
-        console.log(1)
-        if(advisor.role !== "SUPER_ADMIN") return res.status(403).json("FORBIDDEN")
-        console.log("advisor.role:",  advisor.role)
-        return res.status(200).json()
+      const { id } = req.params;
+      //const { teamName, userName } = req.query
+      if (!req.user) {
+        return res.status(401).json({ message: "UNAUTHORIZED" });
+      }
+      const advisor = await this.service.findAdvisorById(id);
+      if (advisor.role !== "SUPER_ADMIN")
+        return res.status(403).json("FORBIDDEN");
+      return res.status(200).json();
     } catch (error) {
       return res.status(500).json({ message: "Something Wrong" });
     }
   }
   //=================================================================================================================================================================================
 
-  async findAdvisors(req: any, res: any) {//TODO: TYPE CONVERT
-    try{
-      const {take, limit} = req.query as any //TODO: TYPE CONVERT
-      if(!req.user){
-        return res.status(401).json({message:"UNAUTHORIZED"})
+  async findAdvisors(req: any, res: any) {
+    //TODO: TYPE CONVERT
+    try {
+      const { take, limit, teamName, userName } = req.query as any; //TODO: TYPE CONVERT
+      if (!req.user) {
+        return res.status(401).json({ message: "UNAUTHORIZED" });
       }
-      if(req.user.role !== "SUPER_ADMIN"){
-        return res.status(403).json({message:"FORBIDDEN"})
+      if (req.user.role !== "SUPER_ADMIN") {
+        return res.status(403).json({ message: "FORBIDDEN" });
       }
-      await this.service.findAdvisors({take, limit})
-      return res.status(200).json()
-    }catch(error){
+      console.log(req.user.role);
+      const result = await this.service.findAdvisors({
+        take,
+        limit,
+        teamName,
+        userName,
+      });
+
+      return res.status(200).json(result);
+    } catch (error) {
       return res.status(500).json({ message: "Something Wrong" });
     }
   }
   //=================================================================================================================================================================================
+  async updatesAdvisor(req: any, res: any) {
+    console.log(req)
+    try {
+      if (!req.user.id)
+        return res.status(401).json({ message: "UNAUTHORIZED" });
 
-  async update() {}
+      const {teamname, username} = req.body //TODO: add fields more after test
+      console.log(teamname, username)
+      await this.service.updatesAdvisor({teamname,username})
+      return res.status(200).json({
+        message:"successfully modified information"
+      })
+    } catch (error) {
+      return res.status(500).json({ message: "Something Wrong" });
+    }
+  }
   //=================================================================================================================================================================================
-
+  async updateMany() {}
+  //=================================================================================================================================================================================
   async delete() {}
+  //=================================================================================================================================================================================
+  async deleteMany() {}
 }
