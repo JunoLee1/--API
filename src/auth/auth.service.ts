bcac72c6dcdb3c6c9928f8d1380eaa71aa5f1880
준오 <juno@jun-oui-noteubug.local>
2026-04-05 13:55:07 +0900

    <feat> auth service 임시
    1. 단일 관리자 찾기
    
    2. 다수 관리자 찾기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import AuthRepo from "./auth.repo";
-import { generateToken } from "../lib/token"
+import { generateToken } from "../lib/token";
 import { LoginInput } from "./auth.DTO";
 //import bcrypt  from "bcrypt";
 export default class AuthService {
   constructor(private repo: AuthRepo = new AuthRepo()) {}
   async signUp({ email, password, confirmPassword, nickname }: any) {
+    //TODO: TYPE CONVERT
     if (!email) {
       throw new Error("INVALID_EMAIL");
     }
@@ -22,34 +23,57 @@ export default class AuthService {
     if (duplicatedNickname) {
       throw new Error("DUPLICATED_NICKNAME");
     }
-    if (password !== confirmPassword) throw new Error("PASSWORD_NOT_MATCH");
-    
+    if (password !== confirmPassword) throw new Error("PASSWORD_NOT_MATCH"); //TODO: hash password
+    //TODO: 휴대폰번호 중복검사후 암호화해서 저장하기
   }
   //=================================================================================================================================================================================
-  async login({email, password }:LoginInput) {
+  async login({ email, password }: LoginInput) {
     const user = await this.repo.findUniqueEmail(email);
 
     if (user === null) throw new Error("NOT FOUND");
-  
-    if (password !== user.password )throw new Error("Wrong Password");
-   
-    const {accessToken, refreshToken}  = await generateToken(user.id)
-  
-    return { accessToken ,refreshToken}
+
+    if (password !== user.password) throw new Error("Wrong Password");
+
+    const { accessToken, refreshToken } = await generateToken(user.id);
+
+    return { accessToken, refreshToken };
   }
   //=================================================================================================================================================================================
   async findAdvisorById(id: any) {
-    const isExistingAdvisor = await this.repo.findAdvisorById(id)
+    //TODO: TYPE CONVERT
+    const user = await this.repo.findAdvisorById(id);
 
-    if (isExistingAdvisor === null){
-        throw new Error("NOT FOUND")
+    if (user === null) {
+      throw new Error("NOT FOUND");
     }
-    return isExistingAdvisor
+    return user;
   }
   //=================================================================================================================================================================================
-  async findAdvisors() {}
+  async findAdvisors({ take, limit, teamname, username }: any) {
+    //TODO: TYPE CONVERT
+    const where: any = {};//TODO: TYPE CONVERT
+    if (teamname) {
+      where.teamname = teamname;
+    }
+
+    if (username) {
+      where.username = username;
+    }
+    const advisors = await this.repo.findAdvisors({
+      where,
+      take,
+      limit,
+    });
+    return advisors;
+  }
   //=================================================================================================================================================================================
-  async update() {}
+  async updatesAdvisor({teamname, username}:any) {
+    return null
+  }
+  //=================================================================================================================================================================================
+  async updateMany() {}
   //=================================================================================================================================================================================
   async delete() {}
+  //=================================================================================================================================================================================
+  async deleteMany() {}
 }
