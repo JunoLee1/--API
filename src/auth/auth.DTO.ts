df1552301b3c967e8a08f68a5a9b5fb97d09cc74
준오 <juno@jun-oui-noteubug.local>
2026-04-09 12:07:06 +0900

    feat: 로그인 인풋/ 아웃풋 타입 지정

--- src/auth/auth.DTO.ts
+++ src/auth/auth.DTO.ts
@@ -2,3 +2,32 @@ export type LoginInput = {
     email:string,
     password: string
 }
+export type Pagenation = {
+    take:number,
+    page: number
+}
+export type NameType = {
+    username:string | undefined,
+    teamname:string | undefined
+}
+export type InputData = {
+    id:number,
+}
+enum Role {
+    ADMIN,
+    SUPER_ADMIN
+}
+export interface IUser {
+    id: number, // should change to string
+    username: string
+    teamname: string,
+    email:string,
+    password: string,
+    //phoneNumber:string
+    role:Role.ADMIN
+    country: String
+}
+export type LoginOutput = {
+    accessToken:string,
+    refreshToken:string
+}
