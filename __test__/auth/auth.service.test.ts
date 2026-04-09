3924718fe0a1206cfcbb31c460d19c7fdead52f1
준오 <juno@jun-oui-noteubug.local>
2026-04-09 12:08:59 +0900

    test: 인증 로직 테스트
    1 .관리자 정보 찾기 테스트(단일/ 다수)추
    2. 관리자 삭제 테스트 (단일/다수) 추가

--- __test__/auth/auth.service.test.ts
+++ __test__/auth/auth.service.test.ts
@@ -9,6 +9,13 @@ const MockRepo = {
   findUniqueEmail: jest.fn(),
   findUnique: jest.fn(),
   findAdvisorById: jest.fn(),
+  findAdvisors: jest.fn(),
+  updatesAdvisor: jest.fn(),
+  updateAdvisors: jest.fn(),
+  updateAdvisorsStatus: jest.fn(),
+  findAdvisorsByIds: jest.fn(),
+  delete: jest.fn(),
+  deleteMany: jest.fn(),
 } as any;
 
 jest.mock("../../src/lib/token", () => ({
@@ -138,23 +145,336 @@ describe("인증 로직 테스트 - login service", () => {
 describe("인증 로직 테스트 - 관리자 정보 찾기", () => {
   test("해당 관리자가 존재하지 않는 경우 404", async () => {
     MockRepo.findAdvisorById.mockResolvedValue(null);
-    await expect(
-      service.findAdvisorById({
-        id: 1,
-      }),
-    ).rejects.toThrow("NOT FOUND");
+    await expect(service.findAdvisorById(1)).rejects.toThrow("NOT FOUND");
+  });
+  test("관리자가 존재 하는 경우 해당 유저 리턴", async () => {
+    MockRepo.findAdvisorById.mockResolvedValue({
+      id: 1,
+      username: "juno",
+      team: "Machester Busan United",
+    });
+    const result = await service.findAdvisorById(1);
+    expect(MockRepo.findAdvisorById).toHaveBeenCalledWith(1);
+    expect(result).toEqual({
+      id: 1,
+      username: "juno",
+      team: "Machester Busan United",
+    });
   });
 });
 
-/*
 //======================================================================================
-describe("인증 로직 테스트 - 회원 정보 수정", () => {
-  test("", async () => {});
-});
-//======================================================================================
-describe("인증 로직 테스트 - 회원 삭제", () => {
-  test("", async () => {});
+describe("인증 로직 테스트 - 관리자들 찾기", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("관리자 이름으로 조회 했을때 없는 경우, []", async () => {
+    MockRepo.findAdvisors.mockResolvedValue([]); // 예상 되는 결과값
+
+    const result = await service.findAdvisors({ page: 1, take: 10 },{ teamname:undefined,username: "junoK" });
+    expect(MockRepo.findAdvisors).toHaveBeenCalled();
+    expect(result).toEqual([]);
+  });
+
+  test("팀으로 관리자 목록을 조회 했을 때 없는 경우, []", async () => {
+    MockRepo.findAdvisors.mockResolvedValue([]); // 예상 되는 결과값
+
+    const result = await service.findAdvisors(
+      { page: 1, take: 10 },
+      { username: undefined, teamname: "JunoFc" },
+    );
+    expect(MockRepo.findAdvisors).toHaveBeenCalled();
+    expect(result).toEqual([]);
+  });
+
+  test("팀, 관리자 이름으로 조회했을 때 없는 경우, []", async () => {
+    MockRepo.findAdvisors.mockResolvedValue([]); // 예상 되는 결과값
+
+    const result = await service.findAdvisors(
+      { page: 1, take: 10 },
+      {
+        username: "junoK",
+        teamname: "JunoFc",
+      },
+    );
+    expect(MockRepo.findAdvisors).toHaveBeenCalled();
+    expect(result).toEqual([]);
+  });
+
+  test("관리자 이름으로 조회 했을때 여러개의 데이터를 배열로 리턴", async () => {
+    const data = [
+      { username: "juno", teamname: "Manchester Ulsan FC" },
+      { username: "juno", teamname: "Chelsea Pusan FC" },
+    ];
+    MockRepo.findAdvisors.mockResolvedValue(data);
+    const result = await service.findAdvisors(
+      { page: 1, take: 10 },
+      {
+        teamname: undefined,
+        username: "juno",
+      },
+    );
+    expect(MockRepo.findAdvisors).toHaveBeenCalled();
+    expect(result).toEqual(data);
+  });
+
+  test("팀으로 관리자 목록을 조회 했을 때 여러개의 데이터를 배열로 리턴", async () => {
+    const data = [
+      { username: "juno", teamname: "Manchester Ulsan FC" },
+      { username: "jenny", teamname: "Manchester Ulsan FC" },
+    ];
+    MockRepo.findAdvisors.mockResolvedValue(data);
+    const result = await service.findAdvisors(
+      { page: 1, take: 10 },
+      {
+        username: undefined,
+        teamname: "Manchester Ulsan FC",
+      },
+    );
+    expect(MockRepo.findAdvisors).toHaveBeenCalled();
+    expect(MockRepo.findAdvisors).toHaveBeenCalledWith({
+      where: {
+        teamname: "Manchester Ulsan FC",
+      },
+      page: 1,
+      take: 10,
+    });
+    expect(result).toEqual(data);
+  });
+
+  test("팀과 관리자 이름으로 조회했을 때 리스트가 있는 경우, 여러개의 데이터 배열로 리턴 ", async () => {
+    const data = [
+      //TODO: 암호화된 휴대폰 번호 결과 값에 넣어야됨
+      { username: "chloe", teamname: "Chelsea Pusan FC" },
+      { username: "chloe", teamname: "Chelsea Pusan FC" },
+      { username: "chloe", teamname: "Chelsea Pusan FC" },
+    ];
+    MockRepo.findAdvisors.mockResolvedValue(data);
+    const result = await service.findAdvisors(
+      {
+        page: 1,
+        take: 10,
+      },
+      {
+        username: "chloe",
+        teamname: "Chelsea Pusan FC",
+      },
+    );
+    expect(MockRepo.findAdvisors).toHaveBeenCalled();
+    expect(MockRepo.findAdvisors).toHaveBeenCalledWith({
+      where: {
+        username: "chloe",
+        teamname: "Chelsea Pusan FC",
+      },
+      page: 1,
+      take: 10,
+    });
+    expect(result).toEqual(data);
+  });
 });
 
 //======================================================================================
-*/
+describe("인증 로직 테스트 - 단일 관리자 정보 수정", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("해당 관리자가 없는 경우 404 던지기", async () => {
+    //GIVEN
+    const input = {
+      id: 2,
+      teamname: "juno Fc",
+      username: "juno",
+    };
+    MockRepo.findAdvisorById.mockResolvedValue(null);
+    MockRepo.updatesAdvisor.mockResolvedValue(null);
+    //WHEN + THEN
+    await expect(service.updatesAdvisor(input.id, input)).rejects.toThrow(
+      "NOT FOUND",
+    );
+  });
+  //test("해당 관리자의 비밀번호 변경시 현재 비밀번호와 같은 경우 400 에러 던지기")
+  test("해당 관리자 정보 변경 성공시 해당 유저 정보 반환", async () => {
+    //  GIVEN
+    const input = {
+      id: 2,
+      username: "JunKi",
+      teamname: "MK DONS",
+      password: "hashed",
+    };
+    const updatedInput = {
+      id: 2,
+      username: "JunKi",
+      teamname: "MK DONS FC",
+      password: "hashed",
+    };
+    //
+    MockRepo.findAdvisorById.mockResolvedValue({
+      id: 2,
+      username: "JunKi",
+      teamname: "MK DONS",
+      password: "hashed",
+    });
+    MockRepo.updatesAdvisor.mockResolvedValue(updatedInput);
+    //  WHEN
+    const result = await service.updatesAdvisor(input.id, input);
+    //  THEN
+    expect(MockRepo.findAdvisorById).toHaveBeenCalled();
+    //expect(MockRepo.updatesAdvisor).toHaveBeenCalledWith(input);
+    expect(result).toEqual(updatedInput);
+  });
+});
+
+//======================================================================================
+describe("인증로직 테스트 - 다수 관리자 상태 정보 수정", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("해당 관리자들이 존재 하지 않는 경우 유효 하지않는 관리자 에러 던지기", async () => {
+    // GIVEN
+    const data: any = []; //DB 에서 가지고온 가짜데이터
+    const inputData = [
+      {
+        id: 1,
+        status: "INACTIVE",
+      },
+    ];
+    MockRepo.findAdvisorsByIds.mockResolvedValue(data);
+    //WHERE + THEN
+    await expect(service.updateAdvisorsStatus(inputData)).rejects.toThrow(
+      "Invalid adminstrators",
+    );
+  });
+  test("filter후 데이터 반환 체크", async () => {
+    // GIVEN
+    const data = [
+      //DB
+      {
+        id: 1,
+        status: "ACTIVE",
+      },
+      {
+        id: 2,
+        status: "ACTIVE",
+      },
+    ];
+    const inputData = [
+      // 클라이언트 요청
+      {
+        id: 1,
+        status: "INACTIVE",
+      },
+      {
+        id: 2,
+        status: "INACTIVE",
+      },
+    ];
+    MockRepo.findAdvisorsByIds.mockResolvedValue(data);
+    MockRepo.updateAdvisorsStatus.mockResolvedValue(inputData);
+    //WHEN
+    const result = await service.updateAdvisorsStatus(inputData);
+
+    //THEN
+    expect(result).toEqual(inputData);
+  });
+});
+
+//======================================================================================
+describe("인증 로직 테스트 - 회원 삭제", () => {
+  beforeEach(() => jest.clearAllMocks());
+  test("해당 관리자가 존재 하지 않는 경우 NOT FOUND 에러 던지기 테스트", async () => {
+    // GIVEN
+    const fake = {
+      id: 1,
+    };
+    MockRepo.findAdvisorById.mockResolvedValue(null);
+    // WHEN + THEN
+    await expect(service.delete(fake.id)).rejects.toThrow("NOT FOUND");
+  });
+  test("삭제한 관리자의 삭제 isDeleted가 true라면 DB까지 삭제", async () => {
+    const fake = {
+      id: 1,
+      username: "juno",
+      isDeleted: true,
+    };
+    MockRepo.findAdvisorById.mockResolvedValue(fake);
+
+    MockRepo.delete.mockResolvedValue(undefined);
+
+    const result = await service.delete(fake.id);
+
+    expect(MockRepo.findAdvisorById).toHaveBeenCalledWith(fake.id);
+
+    expect(result).toEqual(undefined);
+  });
+  test("삭제시 isDeleted가 false라면 soft Delete 수행", async () => {
+    const fake = {
+      id: 1,
+      username: "jk",
+      isDeleted: false,
+    };
+    MockRepo.findAdvisorById.mockResolvedValue(fake);
+    MockRepo.updatesAdvisor.mockResolvedValue(fake.id, { isDeleted: true });
+    await service.delete(fake.id);
+    expect(MockRepo.findAdvisorById).toHaveBeenCalledWith(fake.id);
+    expect(MockRepo.updatesAdvisor).toHaveBeenCalledWith(fake.id, {
+      isDeleted: true,
+    });
+  });
+});
+
+//======================================================================================
+
+describe("인증 로직 테스트 - 다수 회원 삭제", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("해당 삭제할 유저들이 DB에 존재 하지 않는다면 NOT FOUND 에러 메시지 확인", async () => {
+    const DATA = [
+      {
+        id: 1,
+        role: "ADMIN",
+        username: "juno",
+        isDeleted: false,
+        status: "INACTIVE",
+      },
+      {
+        id: 2,
+        role: "ADMIN",
+        username: "jk",
+        isDeleted: false,
+        status: "INACTIVE",
+      },
+    ];
+    MockRepo.findAdvisorsByIds.mockResolvedValue([]);
+    await expect(service.deleteMany(DATA)).rejects.toThrow("NOT FOUND");
+  });
+  test("filter 후 isDeleted  false시, true로 변경", async () => {
+    const requestData = [
+      { id: 2, isDeleted: true },
+      { id: 3, isDeleted: true },
+    ];
+    const userDB = [
+      {
+        id: 2,
+        role: "ADMIN",
+        username: "jk",
+        isDeleted: false,
+      },
+      {
+        id: 3,
+        role: "ADMIN",
+        username: "jik",
+        isDeleted: false,
+      },
+    ];
+    MockRepo.findAdvisorsByIds.mockResolvedValue(userDB);
+    MockRepo.updateAdvisorsStatus.mockResolvedValue(requestData);
+
+    const result = await service.deleteMany(requestData);
+
+    expect(MockRepo.updateAdvisorsStatus).toHaveBeenCalledWith({
+      ids: [2, 3],
+      isDeleted: true,
+    });
+  });
+});
