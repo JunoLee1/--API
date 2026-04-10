86da0dfadb503aa3fa98ed34b756c3c6e9c78e86
준오 <juno@jun-oui-noteubug.local>
2026-04-10 12:48:52 +0900

    feat: auth 컨트롤러 개선
    로그인 타입 개선
    관리자 수정 필드 추가
    관리자다수 수정 API 개선

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,7 +15,6 @@ export default class AuthController {
 
   //=================================================================================================================================================================================
   async login(req: Request, res: Response) {
-    //TODO: TYPE CONVERT
     try {
       const { email, password } = req.body as LoginInput;
       const result = await this.service.login({ email, password });
@@ -27,17 +26,14 @@ export default class AuthController {
   
   //=================================================================================================================================================================================
 
-  async findAdvisorById(req: Request<{id:string},{},{}>, res: Response) {
-    //TODO: TYPE CONVERT
+  async findAdvisorById(req: Request, res: Response) {
     try {
-      const id = Number(req.params.id)
-     
       if (!req.user?.id) {
         return res.status(401).json({ message: "UNAUTHORIZED" });
       }
-      console.log(req.user)
       if (req.user.role !== Role.SUPER_ADMIN)
         return res.status(403).json({ message: "FORBIDDEN" });
+      const id = Number(req.params.id)
       const result = await this.service.findAdvisorById(id);
       return res.status(200).json(result);
     } catch (error) {
@@ -47,7 +43,6 @@ export default class AuthController {
   //=================================================================================================================================================================================
 
   async findAdvisors(req: Request, res: Response) {
-    //TODO: TYPE CONVERT
     try {
       const { take, page, teamname, username } = req.query as any; //TODO: TYPE CONVERT
       if (!req.user?.id) {
@@ -76,7 +71,7 @@ export default class AuthController {
         return res.status(401).json({ message: "UNAUTHORIZED" });
       const id = Number(req.params);
 
-      const { teamname, username, email, password, country, role} = req.body; //TODO: add fields more after test
+      const { teamname, username, email, password, country, role} = req.body;
       await this.service.updatesAdvisor({id, email, teamname, username, password, country, role});
       return res.status(200).json({
         message: "successfully modified information",
@@ -88,7 +83,7 @@ export default class AuthController {
   //=================================================================================================================================================================================
   async updateAdvisorsStatus(req: Request, res: Response) {
     try {
-      if (!req.user)
+      if (!req.user?.id)
         return res.status(401).json({ message: "UNAUTHORISED" });
       if (req.user.role !== "SUPER_ADMIN")
         return res.status(403).json({ message: "FORBIDDEN" });
@@ -110,7 +105,7 @@ export default class AuthController {
         return res.status(403).json({
           message: "FORBIDDEN",
         });
-      const id = req.params;
+      const id = Number(req.params);
       await this.service.delete(id);
 
       return res.status(204).send();
