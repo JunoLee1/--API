963330c6e5b17a59cd0da3fe634f602ada50bee1
준오 <juno@jun-oui-noteubug.local>
2026-04-15 14:15:24 +0900

    feat: country controller
    1. access multiple countries

--- src/country/country.controller.ts
+++ src/country/country.controller.ts
@@ -17,4 +17,26 @@ export default class Controller {
       });
     }
   }
+  async getCountries (req:any, res:any){
+    try{
+      const {name, code, region} = req.query as any
+      let uppper = ""
+      if(!this.isUpperCase(code)){
+        uppper = code.toUpperCase()
+      }
+      const result = await this.service.getCountries(name, code, region)
+      return res.status(200).json({
+        message: "성공적으로 데이터를 가지고 왔습니다.",
+        data: result,
+      });
+    }catch(error){
+     return res.status(500).json({
+        message: "INTERNAL SERVER ERROR",
+      });
+    }
+  }
+  
+  isUpperCase(str:string){
+    return str === str.toUpperCase()
+  }
 }
