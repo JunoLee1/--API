2c3deb8fc7d6bf8bc4fc8aa6750bcfcf207c0c29
준오 <juno@jun-oui-noteubug.local>
2026-04-19 13:37:44 +0900

    feat<test>:
    1. 단일 국가조회 테스트(외부 API 사용 유무)
    
    2. 복수 국가 조회 테스트

--- __test__/country/country.service.test.ts
+++ __test__/country/country.service.test.ts
@@ -1,54 +1,98 @@
 import { describe, test, jest, expect } from "@jest/globals";
-import Service from "../../src/country/country.service"
-
+import Service from "../../src/country/country.service";
+import { CountryApiClient } from "../../src/externalAPI";
 const mockRepo = {
-    getCountryByCode:jest.fn(),
-    getCountries:jest.fn()
+  getCountryByCode: jest.fn(),
+  getCountries: jest.fn(),
 } as any;
 
-const service = new Service(mockRepo)
+const mockCountryApiClient = {
+  getAllCountries: jest.fn(),
+  getCountryByCode: jest.fn(),
+  getCountries: jest.fn(),
+} as any;
+const service = new Service(mockCountryApiClient, mockRepo);
 
 describe("코드로 단일 국가 조회", () => {
-    test("해당 코드가 존재하지 않는 경우 invalid code 나오는지 확인", async() => {
-        //Given
-        const fakeCountry = {
-            code: null
-        }
-        mockRepo.getCountryByCode.mockResolvedValue(null)
-        // When + then
-        await expect(service.getCountryByCode).rejects.toThrow("invalid code")
-    })
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("해당 코드가 존재하지 않는 경우 invalid code 나오는지 확인", async () => {
+    //Given
+    mockRepo.getCountryByCode.mockResolvedValue(null);
+    // When + then
+    await expect(service.getCountryByCode).rejects.toThrow("invalid code");
+  });
+  test("DB 데이터와 외부 데이터가 없는 경우", async () => {
+    mockRepo.getCountryByCode.mockResolvedValue(null);
+    mockCountryApiClient.getCountryByCode.mockResolvedValue(null);
 
-    test("성공적으로 단일 국가가 조회되는지 확인", async() => {
-        // Given
-        const fakeCountry = {
-            code:"KR",
-            name:"South Korea",
-            region: "Asia"
-        }
-        mockRepo.getCountryByCode.mockResolvedValue(fakeCountry)
-        // When 
-        const result = await service.getCountryByCode(fakeCountry.code)
-        // Then
-        expect(result).toBe(fakeCountry)
-    })
-})
-/*
+    await expect(service.getCountryByCode("KR")).rejects.toThrow(
+      "invalid country",
+    );
+  });
+  test("DB에 데이터가 없는 경우, 외부 API사용", async () => {
+    const fakeCountry = {
+      code: "KR",
+      region: "Asia",
+      name: "South Korea",
+    };
+    mockRepo.getCountryByCode.mockResolvedValue(null);
+    mockCountryApiClient.getCountryByCode.mockResolvedValue({
+      code: "KR",
+      region: "Asia",
+      name: "South Korea",
+    });
+    const result = await service.getCountryByCode("KR");
+    expect(result).toEqual(fakeCountry);
+  });
+
+  test("성공적으로 단일 국가가 조회되는지 확인", async () => {
+    // Given
+    const fakeCountry = {
+      code: "KR",
+      name: "South Korea",
+      region: "Asia",
+    };
+    mockRepo.getCountryByCode.mockResolvedValue(fakeCountry);
+    // When
+    const result = await service.getCountryByCode(fakeCountry.code);
+    // Then
+    expect(result).toEqual(fakeCountry);
+  });
+});
+
 describe("복수 국가 조회 테스트", () => {
-    test("", async() => {
-        // Given
-        const mockData = [{
-            code: 
-        }]
-        // When 
-        // Then
-    })
-/*
-    test("", async() => {
-        // Given
-        // When 
-        // Then
-    })
-       
-})
-*/
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("DB 와 외부 API 둘다 데이터가 없는 경우, invalid country 던지기", async () => {
+    mockRepo.getCountries.mockResolvedValue([]);
+    mockCountryApiClient.getAllCountries.mockResolvedValue([]);
+
+    await expect(
+      service.getCountries({ name: "South Korea", code: "KR", region: "Asia" }),
+    ).rejects.toThrow("Invalid Countries");
+  });
+  test("DB엔 데이터가 없지만 외부 API 에 있는경우, 외부 API 값 던지기", async () => {
+    const contries = [
+      {
+        name: "South Korea",
+        code: "KR",
+        region: "Asia",
+      },
+      {
+        name:"Japan",
+        code:"JP",
+        region:"Asia"
+      }
+    ];
+    mockRepo.getCountries.mockResolvedValue([])
+    mockCountryApiClient.getAllCountries.mockResolvedValue(contries);
+
+    const result = await service.getCountries({region:"Asia"})
+
+    expect(result).toEqual(contries)
+  });
+  //test("DB 데이터 있는 경우, 결과값 던지기", async () => {})
+});
