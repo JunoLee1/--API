7eaeaffd7f03daf75ede48a1a6c170075883705b
준오 <juno@jun-oui-noteubug.local>
2026-04-21 11:10:15 +0900

    fix: signup repo
    fix type

--- src/auth/auth.repo.ts
+++ src/auth/auth.repo.ts
@@ -1,13 +1,21 @@
 //import AuthService from "./auth.service";
 //import prisma from "../lib/prisma";
-import type { PrismaClient } from "@prisma/client";
-import { LoginInput, signUpInputDto,signUpInputServiceDto, signUpOutputDto } from "./auth.DTO";
+import { PrismaClient } from "../generated/client";
+import { Role } from "../generated/client";
+import {
+  LoginInput,
+  signUpInputDto,
+  signUpInputServiceDto,
+  signUpOutputDto,
+  encryptedPhoneNumberType,
+  UpdateUserInputDTO,
+} from "./auth.DTO";
+
 export class AuthRepository {
   constructor(private prisma: PrismaClient) {}
 
   //=================================================================================================================================================================================
   async findByEmail(email: string) {
-    
     return await this.prisma.user.findUnique({
       where: { email },
     });
@@ -19,7 +27,7 @@ export class AuthRepository {
     });
     return result;
   }
-  
+
   async isEmailDuplicated(email: string) {
     const user = await this.prisma.user.findUnique({
       where: { email },
@@ -27,40 +35,69 @@ export class AuthRepository {
     return !!user;
   }
   //=================================================================================================================================================================================
-  async findAdvisorById(id:number) {
+  async findAdvisorById(id: number) {
     const user = await this.prisma.user.findUnique({
-      where:{id} 
-    })
-    return user
+      where: { id },
+    });
+    return user;
   }
   //=================================================================================================================================================================================
-  async createAdmin(data:signUpInputServiceDto):Promise<signUpOutputDto>{
+  async createAdmin(data: signUpInputServiceDto): Promise<signUpOutputDto> {
     const newAdmin = await this.prisma.user.create({
-      data:{
-        email:data.email,
-        password: data.email
-      }
-    })
-    return newAdmin
+      data: {
+        email: data.email,
+        password: data.password,
+        nickname: data.nickname,
+        username: data.username,
+        date_of_birth: data.birth_date,
+        role: Role.ADMIN,
+        nationality: {
+          connect: {
+            id: data.country.id,
+          },
+        },
+
+        isDeleted: false,
+        phoneNumber: {
+          create: {
+            iv: data.phoneNumber.iv,
+            encrypted: data.phoneNumber.encrypted,
+          },
+        },
+      },
+      include: {
+        phoneNumber: true,
+        nationality: true,
+      },
+    });
+    return {
+      email: newAdmin.email,
+      username: newAdmin.username,
+      nickname: newAdmin.nickname,
+    };
   }
   //=================================================================================================================================================================================
   async findAdvisorsByIds(ids: number[]) {
-    const admins = await this.prisma.findMany({
-      where:{
-        in:{
-          ids
-        }
-      }
-    })
-    return admins
+    const admins = await this.prisma.user.findMany({
+      where: {
+        in: {
+          ids,
+        },
+      },
+    });
+    return admins;
   }
   //=================================================================================================================================================================================
   async findAdvisors({ where }: any) {
     return [];
   }
   //=================================================================================================================================================================================
-  async updatesAdvisor(data:any) {
-    return null;
+  async updatesAdvisor(data: UpdateUserInputDTO) {
+    const admins = await this.prisma.user.update({
+      where: { id: data.id },
+      data: data,
+    });
+    return admins;
   }
 
   //=================================================================================================================================================================================
