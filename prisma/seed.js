2b242217e1b950d330961550a0166f89be34213d
준오 <juno@jun-oui-noteubug.local>
2026-04-24 17:15:04 +0900

    feat<seed>

---
+++ prisma/seed.js
@@ -0,0 +1,94 @@
+import "dotenv/config";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient, Role, Status } from "../src/generated/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+
+const hashed = await bcrypt.hash("12345678",10)
+async function main() {
+  const Korea = await prisma.country.upsert({
+    where: {
+      code: "KR",
+    },
+    update: {},
+    create: {
+      code: "KR",
+      name: "South Korea",
+    },
+  });
+  const KPL = await prisma.league.upsert({
+    where: {
+      id: 1,
+    },
+    update: {},
+    create: {
+      name: "KPL",
+      nation: {
+        connect: {
+          code: "KR",
+        },
+      },
+    },
+  });
+  const Busan = await prisma.team.upsert({
+    where: {
+      id: 1,
+    },
+    update: {},
+    create: {
+      team_name: "Busan FC",
+      short_name: "BUS",
+      founded: new Date("2000-01-01"),
+
+      league: {
+        connect: { id: 1 },
+      },
+    },
+  });
+  const phone = await prisma.phoneNumber.create({
+    data: {
+      iv: "acd",
+      encrypted: "01011112222",
+    },
+  });
+  const Junmo = await prisma.user.upsert({
+    where: {
+      email: "junmo@test.com",
+    },
+    update: {},
+    create: {
+      id: 1,
+      email: "junmo@test.com",
+      password: hashed,
+      username: "이준모",
+      nationality: {
+        connect: {
+          code: "KR",
+        },
+      },
+      isDeleted: false,
+      status: Status.ACTIVE,
+      date_of_birth: new Date("1996-08-16"),
+      phoneNumber: {
+        connect: {
+          id: phone.id,
+        },
+      },
+      nickname: "JM",
+      team: {
+        connect: {
+          id: 1,
+        },
+      },
+      role: Role.SUPER_ADVISOR,
+    },
+  });
+}
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect());
