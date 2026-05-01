491737396939dc0a1c8b7948dccb126733519d82
준오 <juno@jun-oui-noteubug.local>
2026-05-01 17:41:46 +0900

    <fix> asynchrozation

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -4,7 +4,7 @@ const algorithm = "aes-256-cbc"
 const key  = crypto.randomBytes(32)
 const iv = crypto.randomBytes(16)
 
-export function encrypt (text:string){
+export async function encrypt(text:string){
     const cipher = crypto.createCipheriv(algorithm, key, iv)
     let encrypted =  cipher.update(text,"utf-8","hex")
     encrypted += cipher.final("hex");
