c16b021c1e8d6d82ffdf4e142101c115643acf91
준오 <juno@jun-oui-noteubug.local>
2026-05-02 11:04:39 +0900

    <fix>:user types

--- src/lib/express.d.ts
+++ src/lib/express.d.ts
@@ -4,12 +4,27 @@ declare global {
     interface User {
       id: number;
       username: string;
-      teamname: string;
+      team:Team,
       email: string;
       password: string;
       role: Role; // "SUPER_ADMIN" 대신 Enum 타입을 직접 할당
       country:string;
       isDeleted?: boolean;
+      phoneNumber:PhoneNumber;
+
+    }
+    interface Country {
+      id:number,
+      name:string
+    }
+    interface PhoneNumber {
+      iv:string;
+      encrypted:string
+    }
+    interface Team {
+      id:number
+      team_name: string
+      shorten_name: string
     }
     interface Request {
       user?: User
