7b7bd4724d3a3cc001149047e2558b58aaaa443e
준오 <juno@jun-oui-noteubug.local>
2026-05-02 10:27:24 +0900

    <chore>: generate token

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,14 +1,14 @@
 import { sign, verify } from "jsonwebtoken";
-import {JWT_ACCESS_TOKEN_SECRET, JWT_REFRESH_TOKEN_SECRET}from "./constants" 
+import { JWT_ACCESS_TOKEN_SECRET, JWT_REFRESH_TOKEN_SECRET } from "./constants";
 
 const generateToken = async (id: any) => {
   const accessToken = sign({ sub: id }, JWT_ACCESS_TOKEN_SECRET, {
-    expiresIn:"1h"
+    expiresIn: "1h",
   });
-  const refreshToken = sign({sub: id}, JWT_REFRESH_TOKEN_SECRET, {
-    expiresIn: "1d"
-  })
-  return {accessToken, refreshToken}
+  const refreshToken = sign({ sub: id }, JWT_REFRESH_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+  return { accessToken, refreshToken };
 };
 /*
 const verifyAccessToken = async (token: any) => {
@@ -21,4 +21,4 @@ const verifyRefreshToken = async (token: any) => {
     return { sub: decode.sub };
 };
 */
-export {generateToken };
+export { generateToken };
