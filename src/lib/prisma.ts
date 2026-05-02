8d96d96e9985043fdc62354b61710c91b7b13af4
준오 <juno@jun-oui-noteubug.local>
2026-05-02 11:05:06 +0900

    <chore>: export client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-let prisma: PrismaClient;
+export let prisma: PrismaClient;
 
 export function getPrisma() {
   if (!prisma) {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
     });
-
     prisma = new PrismaClient({ adapter });
   }
   return prisma;
 }
+export { PrismaClient };
