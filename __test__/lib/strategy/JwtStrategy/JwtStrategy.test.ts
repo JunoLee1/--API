691d7b011202e790d13d49dbf2f6c3df0592e34a
준오 <juno@jun-oui-noteubug.local>
2026-05-08 15:49:31 +0900

    refactor:권한 추가

--- __test__/lib/strategy/JwtStrategy/JwtStrategy.test.ts
+++ __test__/lib/strategy/JwtStrategy/JwtStrategy.test.ts
@@ -5,7 +5,7 @@ import { jwtVerify } from "../../../../src/lib/strategy/JwtStrategy/jwtStrategy"
 describe("jwt verify function",() => {
     test("if it works, send payload down", async() => {
         const done =jest.fn()
-        await jwtVerify({sub:1}as any, done)
+        await jwtVerify({sub:1, role:Role.ADMIN }as any, done)
         expect(done).toHaveBeenCalledWith(null,{id:1, role:Role.ADMIN})
     })
 })
